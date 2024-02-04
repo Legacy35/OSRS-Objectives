d904e8565075e3f1c420c5bb0d8c29516cf20182
Legacy35 <aarongonzalez@ufl.edu>
2024-02-03 20:19:11 -0500

    First commit

--- src/main/java/org/legacy/utils/ObjectivesPlugin.java
+++ src/main/java/org/legacy/utils/ObjectivesPlugin.java
@@ -1,19 +1,12 @@
 package org.legacy.utils;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.widgets.Widget;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.legacy.data.BankData;
-import net.runelite.client.plugins.bank.BankPlugin;
-import org.legacy.utils.ObjectivesConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,6 +42,8 @@ public class ObjectivesPlugin extends Plugin
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			BankData playerValue = new BankData();
+			log.info("Example started!"+ playerValue.getTotalValue());
 		}
 	}
 
